05bf4433321e7316e884a2ca10682364e81bd8e0
José Roberto Júnior <112019067+robertojunior28@users.noreply.github.com>
2024-08-26 19:40:06 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import cv2
+import mediapipe as mp
+
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands()
+mp_drawing = mp.solutions.drawing_utils
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        break
+
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    results = hands.process(rgb_frame)
+
+    if results.multi_hand_landmarks:
+        for landmarks in results.multi_hand_landmarks:
+            for landmark in landmarks.landmark:
+                x = int(landmark.x * frame.shape[1])
+                y = int(landmark.y * frame.shape[0])
+                cv2.circle(frame, (x, y), 5, (0, 255, 0), -1)
+
+    cv2.imshow('Hand Landmarks', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
---
+++ readme.md
@@ -0,0 +1,19 @@
+### 1- comando para instalar o venv (se der erro é pq não precisava dele, passe para o próximo)
+
+python -m venv venv
+
+
+### 2- comando para navegar até o ambiente virtual
+
+.\venv\Scripts\activate
+
+
+### 3- comando para instalar as libs listadas no requirements.txt
+
+pip install -r requirements.txt
+
+
+### 4- comando para rodar a aplicação 
+
+python main.py
+
